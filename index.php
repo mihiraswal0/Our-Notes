1498f819f9e58337899440d7b7ce3e164bd53f78
mihiraswal0 <91006719+mihiraswal0@users.noreply.github.com>
2022-04-07 15:09:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="styling.css" rel="stylesheet">
+      <link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+    <title>My Notes</title>
+  </head>
+  <body>
+    <!-- Navigation bar -->
+   
+    <nav role="navigation" class="navbar navbar-custom navbar-fixed-top" >
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand">Online Notes</a>
+            <button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div class="navbar-collapse collapse" id="navbarCollapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Login</a></li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+    <script src="https://code.jquery.com/jquery-2.1.3.js"></script>
+<script src="js/bootstrap.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
