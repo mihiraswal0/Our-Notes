5d725442aca8717940a1dae16e4650ab0d3ec326
mihiraswal0 <91006719+mihiraswal0@users.noreply.github.com>
2022-04-10 12:47:58 +0530

    Add files via upload

--- loadnotes.php
+++ loadnotes.php
@@ -22,8 +22,12 @@ if($result = mysqli_query($link, $sql)){
             $time = $row['time'];
             $time = date("F d, Y h:i:s A", $time);
             
-            echo "
-            <div>
+             echo "
+        <div class='note'>
+            <div class='col-xs-5 col-sm-3 delete'>
+                <button class='btn-lg btn-danger' style='width:100%'>delete</button>
+            
+            </div>
             <div class='noteheader' id='$note_id'>
                 <div class='text'>$note</div>
                 <div class='timetext'>$time</div>    
